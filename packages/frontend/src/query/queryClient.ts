6b6b77cadbea86af8e0c06c1a85f5f446e3e601b
WolverinDEV <git@did.science>
2026-03-27 01:31:26 +0100

    chore: disabled query retries on failures
    The client should reload the page on his own if needed.

--- packages/frontend/src/query/queryClient.ts
+++ packages/frontend/src/query/queryClient.ts
@@ -1,15 +1,17 @@
 import { QueryClient } from '@tanstack/react-query'
 
 export function createQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnMount: false,
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: false
-      },
-    }
-  })
+    return new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnMount: false,
+                refetchOnWindowFocus: false,
+                refetchOnReconnect: false,
+
+                retry: false,
+            },
+        }
+    })
 }
 
 export const queryClient = createQueryClient()
